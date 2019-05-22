3d61e8fba04e963eebbfef0929fe8c7acbfa6cb8
Pinpang Liao <pinpang@aliyun.com>
2019-05-22 14:56:55 +0800

    Update tool.py
    去掉多余的import语句；使用列表生成式更新listUiFile()；更新变量命名以回避内置函数名称

--- Chapter03/tool.py
+++ Chapter03/tool.py
@@ -7,22 +7,17 @@
 '''
 
 import os 
-import os.path 
+# import os.path 
 
 # UI文件所在的路径 
-dir = './'  
+root = './'  # 不使用与内置函数同名的变量命名
 
 # 列出目录下的所有ui文件
-def listUiFile(): 
-	list = []
-	files = os.listdir(dir)  
-	for filename in files:  
-		#print( dir + os.sep + f  )
-		#print(filename)
-		if os.path.splitext(filename)[1] == '.ui':
-			list.append(filename)
-	
-	return list
+def listUiFile():
+	return [f for f in os.listdir(root) if f.endswith('.ui')]
+	# 如果文件数量巨大，考虑使用下面的生成器作为返回值
+	# return (f for f in os.listdir(root) if f.endswith('.ui'))
+
 
 # 把后缀为ui的文件改成后缀为py的文件名	
 def transPyFile(filename): 
@@ -30,8 +25,8 @@ def transPyFile(filename):
 
 # 调用系统命令把ui转换成py
 def runMain():
-	list = listUiFile()
-	for uifile in list :
+	uiList = listUiFile()
+	for uifile in uiList :
 		pyfile = transPyFile(uifile)
 		cmd = 'pyuic5 -o {pyfile} {uifile}'.format(pyfile=pyfile,uifile=uifile)  
 		#print(cmd)
